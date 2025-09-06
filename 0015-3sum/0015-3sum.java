ca9fcc40aee02ef69576c288577e8680391a8cb0
Mamtha R <166887560+Mamtha2004@users.noreply.github.com>
2025-09-06 21:48:10 +0530

    Time: 30 ms (70.44%), Space: 51.5 MB (56.93%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,36 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        if (nums.length < 3) {
-            return new ArrayList<>();
-        }
-
-        Set<List<Integer>> result = new HashSet<>();
-        Arrays.sort(nums); // Sort the array
-
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);  // Step 1: sort the array
+        
         for (int i = 0; i < nums.length - 2; i++) {
-            int j = i + 1;
-            int k = nums.length - 1;
+            // Skip duplicates for i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
 
                 if (sum == 0) {
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[i]);
-                    list.add(nums[j]);
-                    list.add(nums[k]);
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
-                    result.add(list);
-                    j++;
-                    k--;
-                } else if (sum > 0) {
-                    k--;
+                    // Skip duplicates for left and right
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;  // need bigger sum
                 } else {
-                    j++;
+                    right--; // need smaller sum
                 }
             }
         }
-
-        return new ArrayList<>(result);
+        return result;
     }
 }
